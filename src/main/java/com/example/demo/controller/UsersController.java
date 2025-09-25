920ee75a0e1e005b1c080d9828d14108c8141fdc
flyingfish032 <hussaincme@gmail.com>
2025-09-25 08:27:46 +0530

    Update UsersController.java

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.Users;
 import com.example.demo.model.UsersManager;
 
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/users")
 public class UsersController {
